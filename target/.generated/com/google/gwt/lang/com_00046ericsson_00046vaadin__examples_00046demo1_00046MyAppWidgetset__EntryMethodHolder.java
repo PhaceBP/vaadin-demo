467716c5d9cbea4cbd2e9ea7c92b9ae099d1dded
Attila Balogh Biro <attila.balogh.biro@ericsson.com>
2016-07-15 15:17:43 +0200

    first commit

---
+++ target/.generated/com/google/gwt/lang/com_00046ericsson_00046vaadin__examples_00046demo1_00046MyAppWidgetset__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class com_00046ericsson_00046vaadin__examples_00046demo1_00046MyAppWidgetset__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
---
+++ target/demo1-1.0-SNAPSHOT/WEB-INF/classes/VAADIN/widgetsets/com.ericsson.vaadin_examples.demo1.MyAppWidgetset/deferredjs/144E4C631575B474138E5327C9FFC8C2/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_ericsson_vaadin_examples_demo1_MyAppWidgetset.runAsyncCallback2('rdb(1599,1,r_d);_.vc=function Rgc(){V1b((!O1b&&(O1b=new $1b),O1b),this.a.d)};SUd(Th)(2);\n//# sourceURL=com.ericsson.vaadin_examples.demo1.MyAppWidgetset-2.js\n')
